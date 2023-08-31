b4a7ee8a949134878f0d168ad679e29180f5212e
Rajasekharpera <116863029+Rajasekharpera@users.noreply.github.com>
2023-08-31 11:44:58 +0530

    save

--- src/components/Heart.tsx
+++ src/components/Heart.tsx
@@ -6,15 +6,16 @@ interface Props {
   icon: string
   alt: string
 }
-
-const Heart: FC<Props> = ({ icon, alt }) => {
-  return <HeartIcon src={icons[icon]} alt={alt} />
-}
-
 const HeartIcon = styled.img({
   width: '17px',
   height: '15px',
   alignSelf: 'center',
 })
+
+const Heart: FC<Props> = ({ icon, alt }) => {
+  return <HeartIcon src={icons[icon]} alt={alt} />
+}
+
+
 
 export default Heart
